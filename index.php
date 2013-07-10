cad561c62c4f1ffad17eb8e7f9b9452303ad97a2
Jarrod Birch <jarrodsbirch95@gmail.com>
2013-07-10 14:26:00 +0100

    Small changes to the homepage

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
 <html>
 <head>
 	<title><?php tmcms_head(); ?></title>
+	<link rel="stylesheet" type="text/css" href="/assets/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600' rel='stylesheet' type='text/css'>
 </head>
 <body>
 <!-- place the below code block inside your main div, section or container etc. -->
-<div style="width:80%;margin:0 auto;">
+<div class="container">
 <?php if(post_check() == true) {
 	show_the_post();
 } else { ?>
-	<h1>This is the homepage.</h1>
+	<h2><?php echo $config['title'];?></h2>
+	<h4>Latest posts</h4>
 	<?php feed_posts();?>
 
 
@@ -24,8 +27,9 @@
 
 
 <?php } ?>
-<div>
-	This div shows on both the homepage and the post page.
+
+<div class="footer">
+<?php show_social();?>
 </div>
 </div>
 
